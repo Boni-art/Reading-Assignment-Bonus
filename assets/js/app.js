b55d6db5d97fe2fdec480f666f0386f184be6741
Biniyam <se.biniyam.abiy@gmail.com>
2021-02-14 23:42:07 +0300

    the functionality that calculate a persons BMI added to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,3 +42,54 @@ Reflect.setPrototypeOf(text.prototype, showContent);
 function tags(strings, ...names) {
     return names.join(' ');
 }
+
+
+(function init(){
+    let choice = prompt("What do you want to do? choose.\n 1. Calculate a persons BMI\n 2. See persons whose BMI were calculated in this program with their BMI.");
+    if (choice == 1) {
+        let personFirstName = prompt("Enter the persons informations first.\n Enter his or her first name.");
+        let personLastName = prompt("Enter his or her last name.");
+        let check = true;
+        while (check) {
+            var personId = prompt("Enter any Id that you haven't given before. This will prompt again if you have entered an id already existing.");
+            if(idChecker.has(personId)){
+                check = true;
+            }else{
+                check = false;
+                idChecker.add(personId)
+            }
+        }
+        let personToCalculateInList = [];
+        Reflect.set(personToCalculateInList, 0, personFirstName);
+        Reflect.set(personToCalculateInList, 1, personLastName);
+        let personToCalculate = Reflect.construct(person, personToCalculateInList);
+        if(Reflect.defineProperty(personToCalculate, 'id', {value: personId})){
+            console.log("property entered.");
+        }
+        persondb.set(personId, personToCalculate);
+        let hChoice = prompt("choose from common heights or enter your custom.\n 1. 1.50\n 2. 1.60\n 3. 1.70\n 4. Enter custom");
+        if (hChoice == 1) {
+            var bmi = BMI(heightSquare(1.50));
+        } else if(hChoice == 2) {
+            var bmi = BMI(heightSquare(1.60));
+        }else if (hChoice == 3) {
+            var bmi = BMI(heightSquare(1.70));
+        }else if(hChoice == 4){
+            let height = prompt("Enter the person's height in meters");
+            var bmi = BMI(heightSquare(height));
+        }else{
+            alert("wrong input try again.");
+        }
+        
+        let weight = prompt("Enter the person's weight");
+        let newText = new text(`${Reflect.get(personToCalculateInList, 0)} ${Reflect.get(personToCalculateInList, 1)}'s Bmi index is ${bmi(weight)}`);
+        newText.view();
+        // console.log(Reflect.getPrototypeOf(newText) + "\n");
+
+        console.log(personToCalculate, idChecker, persondb);  
+        init();
+    }
+
+
+
+})();    
